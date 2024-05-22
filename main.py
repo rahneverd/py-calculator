53b24d6d434b29ec6bcb8df67b2f08131010caa6
rahneverd <rahneverd@gmail.com>
2024-05-22 22:50:57 +0500

    done

---
+++ main.py
@@ -0,0 +1,25 @@
+def get_input():
+  first_num = input("Enter first number: ")
+  second_num = input("Enter second number: ")
+  option = input("Select a choice(+, -, *, /): ")
+  inputs = {"first": first_num, "second": second_num, "option": option}
+  return inputs
+
+def calculate():
+  first_num = int(inputs["first"])
+  second_num = int(inputs["second"])
+  option = inputs["option"]
+  if option == "+":
+    return first_num + second_num
+  elif option == "-":
+    return first_num - second_num
+  elif option == "*":
+    return first_num * second_num
+  elif option == "/":
+    return first_num / second_num
+  else:
+    return "Invalid option"
+
+inputs = get_input()
+result = calculate()
+print(inputs["first"], inputs["option"], inputs["second"], '=', result)
